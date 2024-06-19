cbc9944832526beac37d051e9f21602b1831131f
14nn0l1v31r4 <iannmolendolff@gmail.com>
2024-06-18 21:25:29 -0300

    User update

--- src/main/java/com/iann_java/curso/resources/UserResource.java
+++ src/main/java/com/iann_java/curso/resources/UserResource.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.iann_java.curso.entities.User;
 import com.iann_java.curso.services.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -58,4 +60,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User>update(@PathVariable Long id, @RequestBody User obj) {
+	obj = service.update(id, obj);
+	return ResponseEntity.ok().body(obj);
+	}
 }
